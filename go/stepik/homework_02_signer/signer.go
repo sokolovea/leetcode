09d21991ad2308ac74de8e482265734555628dda
sokolovea <faositb@yandex.ru>
2026-01-23 14:27:00 +0300

    Go: stepik: hw2 44s->8s

--- go/stepik/homework_02_signer/signer.go
+++ go/stepik/homework_02_signer/signer.go
@@ -8,35 +8,49 @@ import (
 	"sync"
 )
 
-// type endOfChan {}
+var mutexCrc32 sync.Mutex
 
 var SingleHash job = func(in, out chan interface{}) {
-	fmt.Printf("SingleHash!!!\n");
-	// InputElemsCount := len(in)
-	// fmt.Printf("InputElemsCount = %v!!!\n", InputElemsCount);
+	fmt.Printf("SingleHash!!!\n")
+	var wg sync.WaitGroup
 	for {
 		data, ok := <-in
 		if !ok {
 			fmt.Println("Канал SingleHash закрыт")
 			break
 		}
-		fmt.Printf("SingleHash get data!\n");
+		fmt.Printf("SingleHash get data!\n")
 		dataString, ok := data.(string)
 		if !ok {
 			dataInt, ok := data.(int)
 			if !ok {
-				fmt.Printf("SingleHash not ok!\n");
+				fmt.Printf("SingleHash not ok!\n")
 				continue
 			}
 			dataString = strconv.Itoa(dataInt)
 		}
-		fmt.Printf("SingleHash: data = %v \n", dataString);
-		out <- DataSignerCrc32(dataString) + "~" + DataSignerCrc32(DataSignerMd5(dataString))
+		fmt.Printf("SingleHash: data = %v \n", dataString)
+		mutexCrc32.Lock()
+		preResult := DataSignerMd5(dataString)
+		mutexCrc32.Unlock()
+		wg.Add(2)
+		var firstSum string
+		var secondSum string
+		go func() {
+			firstSum = DataSignerCrc32(dataString)
+			wg.Done()
+		}()
+		go func() {
+			secondSum = DataSignerCrc32(preResult)
+			wg.Done()
+		}()
+		wg.Wait()
+		out <- firstSum + "~" + secondSum
 	}
 }
 
 var MultiHash job = func(in, out chan interface{}) {
-	fmt.Printf("MultiHash!!!\n");
+	fmt.Printf("MultiHash!!!\n")
 	preResult := make([]string, 6)
 	for {
 		data, ok := <-in
@@ -47,20 +61,26 @@ var MultiHash job = func(in, out chan interface{}) {
 		fmt.Printf("MultiHash get data!\n")
 		dataString, ok := data.(string)
 		if !ok {
-			fmt.Printf("MultiHash data not ok!\n");
+			fmt.Printf("MultiHash data not ok!\n")
 			continue
 		}
+		var wg sync.WaitGroup
+		wg.Add(6)
 		for th := 0; th <= 5; th++ {
-			preResult[th] = DataSignerCrc32(strconv.Itoa(th) + dataString)
+			go func() {
+				preResult[th] = DataSignerCrc32(strconv.Itoa(th) + dataString)
+				wg.Done()
+			}()
 		}
+		wg.Wait()
 		preResult := strings.Join(preResult, "")
-		fmt.Printf("MultiHash: data = %v \n", preResult);
+		fmt.Printf("MultiHash: data = %v \n", preResult)
 		out <- preResult
 	}
 }
 
 var CombineResults job = func(in, out chan interface{}) {
-	fmt.Printf("CombineResults!!!\n");
+	fmt.Printf("CombineResults!!!\n")
 	var allData []string = make([]string, 0, 200)
 	for {
 		data, ok := <-in
@@ -68,10 +88,10 @@ var CombineResults job = func(in, out chan interface{}) {
 			fmt.Println("Канал CombineResults закрыт")
 			break
 		}
-		fmt.Printf("CombineResults get data!\n");
+		fmt.Printf("CombineResults get data!\n")
 		dataString, ok := data.(string)
 		if !ok {
-			fmt.Printf("CombineResults data not ok!\n");
+			fmt.Printf("CombineResults data not ok!\n")
 			continue
 		}
 		allData = append(allData, dataString)
@@ -79,15 +99,15 @@ var CombineResults job = func(in, out chan interface{}) {
 	sort.Slice(allData, func(i, j int) bool {
 		return allData[i] < allData[j]
 	})
-	str := strings.Join(allData, "_") 
-	fmt.Printf("CombineResults = %v\n", str);
+	str := strings.Join(allData, "_")
+	fmt.Printf("CombineResults = %v\n", str)
 	out <- str
 }
 
 func ExecutePipeline(jobs ...job) {
-	fmt.Printf("ExecutePipeline!!!\n");
-	channels := make([](chan interface{}), len(jobs) + 1)
-	for i := 0; i < len(jobs) + 1; i++ {
+	fmt.Printf("ExecutePipeline!!!\n")
+	channels := make([](chan interface{}), len(jobs)+1)
+	for i := 0; i < len(jobs)+1; i++ {
 		channels[i] = make(chan interface{}, 200)
 	}
 
@@ -95,10 +115,10 @@ func ExecutePipeline(jobs ...job) {
 	var wg sync.WaitGroup
 	wg.Add(len(jobs))
 	close(channels[0])
-	for i, job := range(jobs) {
-		go func(){
-			job(channels[i], channels[i + 1])
-			close(channels[i + 1])
+	for i, job := range jobs {
+		go func() {
+			job(channels[i], channels[i+1])
+			close(channels[i+1])
 			fmt.Println("job done!!!!!!!!!!!!!!!!!!!!!!!!!")
 			wg.Done()
 		}()
